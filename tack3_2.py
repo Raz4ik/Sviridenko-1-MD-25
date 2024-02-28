5707a7f4f72782db30d3ce38555112083650b456
Raz4ik <volnov.valerii@yandex.ru>
2024-02-28 12:42:48 +0300

    lab2

--- tack3_2.py
+++ tack3_2.py
@@ -1,12 +1,14 @@
-word = input("Введите слово чтобы проверить на редкость: ")
-bool = 0
+word = ""
+while word != "stop":
+    bool = 0
+    word = input("Введите слово чтобы проверить на редкость: ")
+    for a in word:
+        if a == "ф":
+            bool = 1
+            break
 
-for a in word:
-    if a == "ф":
-        bool = 1
-        break
-
-if bool == 1:
-    print("Ого! Это слово редкое")
-else:
-    print("Эх, это не очень редкое слово")
+    if bool == 1:
+        print("Ого! Это слово редкое")
+    else:
+        print("Эх, это не очень редкое слово")
+    print("Хотите закончить введите stop")
--- task4_2.py
+++ task4_2.py
@@ -1,13 +1,15 @@
 import random
 bool = 0
+c = 0
 while bool != 3:
-    x = random.randint(1,9)
-    y = random.randint(1,9)
+    x = random.randint(-9,9)
+    y = random.randint(-9,9)
     print(x," + ",y," = ", end= " ")
     ans = int(input())
     if ans != x + y:
         bool += 1
         print("Ответ неверный")
     else:
+        c += 1
         print("Правильно")
-print("Увы! Вы проиграли")
+print("Увы! Вы проиграли кол-во правильных ответов = ",c)
